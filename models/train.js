a6f218e53ca16b5262580158382869f054b1ea92
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2019-12-22 23:41:11 +0530

    Made database model more flexible

--- models/train.js
+++ models/train.js
@@ -5,10 +5,10 @@ var TrainSchema = new Schema({
   x_coordinate: { type: Number, required: true, max: 100 },
   y_coordinate: { type: Number, required: true, max: 100 },
   at_distance_1: { type: Number, required: false, max: 100 },
-  at_distance_2: { type: Number, required: true, max: 100 },
+  at_distance_2: { type: Number, required: false, max: 100 },
   at_distance_3: { type: Number, required: false, max: 100 },
   rssi_value_1: { type: Number, required: false, max: 100 },
-  rssi_value_2: { type: Number, required: true, max: 100 },
+  rssi_value_2: { type: Number, required: false, max: 100 },
   rssi_value_3: { type: Number, required: false, max: 100 }
 });
 
