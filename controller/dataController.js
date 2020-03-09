4d9bceac6ac23f3b41eedce96d38054fed90ca3e
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-03-09 15:24:21 +0530

    Changed Model of device to be flexible

--- controller/dataController.js
+++ controller/dataController.js
@@ -1,33 +1,25 @@
 var Device = require('./../models/device');
 
 exports.post = function(req, res, next) {
-  const Data = Object.values(req.body);
-  for (var device in Data) {
-    new Device(Data[device]).save().catch(err => {
-      console.log(err.message);
-    });
-  }
+  const readings = Object.values(req.body);
+  const result = readings.reduce(
+    (acc, { name, region, rssi }) => ({ ...acc, region, [name]: rssi }),
+    {}
+  );
+  const device = new Device(result);
+  device.save(function(err, device) {
+    if (err) return res.send(err);
+    console.log(device);
+  });
   res.status(200).json('Good!');
 };
 
 exports.demo = function(req, res, next) {
-  const Data = Object.values(req.body);
-  console.log(Data);
-  const Data1 = JSON.stringify(req.body);
-  console.log(Data1);
-  console.log(req.body);
-  const result = Data.reduce((acc, item) => {
-    acc['region'] = item.region;
-    acc[item.name] = item.rssi;
-    return acc;
-  }, {});
+  const readings = Object.values(req.body);
+  const result = readings.reduce(
+    (acc, { name, region, rssi }) => ({ ...acc, region, [name]: rssi }),
+    {}
+  );
   console.log(result);
-  let obj = {};
-  for (i in Data) {
-    obj.region = Data[i].region;
-    obj[Data[i].name] = Data[i].rssi;
-  }
-  console.log(obj);
-
   res.status(200).json('Good!');
 };
--- models/device.js
+++ models/device.js
@@ -1,12 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var DeviceSchema = new Schema({
-  name: { type: String, required: true },
-  id: { type: String, required: true, max: 100 },
-  region: { type: String, required: true },
-  rssi: { type: String, required: true }
-});
+var DeviceSchema = new Schema({}, { strict: false });
 
 // Export model.
 module.exports = mongoose.model('Device', DeviceSchema);
