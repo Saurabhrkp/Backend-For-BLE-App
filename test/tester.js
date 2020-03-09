987d784d0340b6bc2df4e2d46d81b17f0e4d2aed
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-03-09 21:00:58 +0530

    Tested BrainJS in Express

--- test/tester.js
+++ test/tester.js
@@ -5,13 +5,15 @@ var dataset = [
   [2, 2, 2]
 ];
 
+var dataObject = { r: 1, g: 0.4, b: 0 };
+
 const postHandler = () => {
   fetch('http://192.168.1.103:3000/demo', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(dataset)
+    body: JSON.stringify(dataObject)
   })
     .then(response => response.json())
     .then(result => {
