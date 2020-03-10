e46c80da793a32c72cef49dec428d656ab4ceef1
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-03-10 15:20:18 +0530

    Successful in prediction of region through knn

--- controller/dataController.js
+++ controller/dataController.js
@@ -1,23 +1,8 @@
 const Device = require('./../models/device');
-
-const model = require('../test/knn.json');
 const KNN = require('ml-knn');
 
+const model = require('../test/knn.json');
 const knn = KNN.load(model);
-
-// const brain = require('brain.js');
-// const model = require('../test/BrainJS/brain.json');
-
-// provide optional config object (or undefined). Defaults shown.
-// const config = {
-//   binaryThresh: 0.5,
-//   hiddenLayers: [3], // array of ints for the sizes of the hidden layers in the network
-//   activation: 'sigmoid', // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh'],
-//   leakyReluAlpha: 0.01 // supported for activation type 'leaky-relu'
-// };
-
-// const net = new brain.NeuralNetwork(config);
-// const JSON = net.fromJSON(model);
 
 exports.post = function(req, res, next) {
   const readings = Object.values(req.body);
@@ -34,10 +19,19 @@ exports.post = function(req, res, next) {
 };
 
 exports.demo = function(req, res, next) {
-  console.log(req.body);
-  const ans = knn.predict(req.body);
+  const readings = Object.values(req.body);
+  const result = readings.reduce(
+    (acc, { name, rssi }) => ({ ...acc, [name]: rssi }),
+    {}
+  );
+  console.log(result);
+  const { Redmi6Pro, RedmiNote5, 'Redmi 7': Redmi7 } = result;
+  const data = { b1: RedmiNote5, b2: Redmi6Pro, b3: Redmi7 };
+  reads = Object.values(data);
+  console.log(reads);
+  const ans = knn.predict(reads);
   console.log(ans);
   // const output = JSON.run(req.body);
   // console.log(output);
-  res.status(200).json('Good!');
+  res.status(200).json(ans);
 };
